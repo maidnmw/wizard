dbd384710c15477cfabc623937c58979e92f599d
Daniil Peshkov <peshkov_d@goldapple.ru>
2021-02-25 18:57:08 +0500

    Поменяли подключение урлов у DRF

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,10 +1,8 @@
 from django.urls import include, path
 from django.contrib import admin
-from rest_framework import routers
 
-router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),
+    path('api/v1/', include('rest_framework.urls')),
 ]
